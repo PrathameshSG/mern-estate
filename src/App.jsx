776d4268f7867818f65116158a2382acd9d281f3
prathamesh <prathameshgaidhani4@gmail.com>
2024-07-01 11:21:53 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline bg-slate-500">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
+
+          {/* <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route
+              path="/update-listing/:listingId"
+              element={<UpdateListing />}
+            />
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
