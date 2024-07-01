a1c39c59716db39e065c2f2913279212b1becbc4
prathamesh <prathameshgaidhani4@gmail.com>
2024-07-01 12:02:51 +0530

    create and run ther server

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/user.route.js";
+// import authRouter from "./routes/auth.route.js";
+// import listingRouter from "./routes/listing.route.js";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+
+// inistlize express
+const app = express();
+
+// listen app
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
